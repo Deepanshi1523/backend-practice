36007a02c33b835214c9d7314bbb23ada4a59ec5
Deepanshi Singhal <deepanshisinghal1523@gmail.com>
2024-09-05 01:24:52 +0530

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-const { error } = require('console');
-const fs = require('fs');
+// const { error } = require('console');
+// const fs = require('fs');
 
-fs.readFile("./bye.txt",'utf8',function(err,data){
-    if(err) console.error(error.message);
-    else console.log(data);
+// fs.readFile("./bye.txt",'utf8',function(err,data){
+//     if(err) console.error(error.message);
+//     else console.log(data);
+// })
+
+const http = require('http');
+
+const server= http.createServer(function(req,res){
+    res.end("hello world");
 })
+
+server.listen(3000);
