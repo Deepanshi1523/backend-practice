3c21308240822da642850f46448adffa3f10556a
Deepanshi Singhal <deepanshisinghal1523@gmail.com>
2024-09-04 20:15:44 +0530

    copy file

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const { error } = require('console');
 const fs = require('fs');
 
-fs.copyFile("hello.txt","/copy/bye.txt",function(err){
+fs.unlink("hello.txt",function(err){
     if(err) console.error(error.message);
     else console.log("done");
 })
